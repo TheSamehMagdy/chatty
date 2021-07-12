0b995cf9d639b9e0721193cdc960a8e40540958d
Sameh Magdy <thesamehmagdy@gmail.com>
2021-07-12 21:25:15 +0200

    Emit sendMessage event when new message is submitted

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,3 +5,10 @@ const socket = io();
 socket.on('message', (msg) => {
 	console.log(msg);
 });
+
+const messageForm = document.querySelector('#message-form');
+messageForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const message = document.querySelector('#message-input').value;
+	socket.emit('sendMessage', message);
+});
