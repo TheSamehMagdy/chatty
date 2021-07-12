440edca23e5f1ca97973c20499c5ec29aad5a680
Sameh Magdy <thesamehmagdy@gmail.com>
2021-07-12 21:35:20 +0200

    Broadcast a message when a user leaves the chat.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ io.on('connection', (socket) => {
 	socket.on('sendMessage', (message) => {
 		io.emit('message', message);
 	});
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left the chat.');
+	});
 });
 
 server.listen(port, () => {
